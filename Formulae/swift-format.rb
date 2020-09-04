8057e44f63f5effd8340abb48fe2d1b659d1f729
St3ff Wo3 <st3ff@wo3.org>
2020-09-04 10:45:30 +0200

    initial commit: swift-format for swift 5.2

---
+++ Formulae/swift-format.rb
@@ -0,0 +1,17 @@
+class SwiftFormat < Formula
+    desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
+    homepage "https://github.com/apple/swift-format"
+    url "https://github.com/apple/swift-format.git", :branch => "swift-5.2-branch"
+    version "5.2.1"
+
+    depends_on :xcode => ["11.4", :build]
+
+    def install
+        system "swift build -c release --disable-sandbox --build-path '.build'"
+        bin.install ".build/release/swift-format"
+    end
+
+    test do
+        system "#{bin}/swift-format --version"
+    end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Formulae
+
+## Setup
+
+```bash
+brew tap st3ffwo3/formulae
+```
+
+## Available Taps
+
+### [swift-format](https://github.com/apple/swift-format)
+> `swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations.
+
+```bash
+brew install swift-format
+```
